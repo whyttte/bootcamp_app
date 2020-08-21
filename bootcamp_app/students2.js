2040900ff9d198b18585858c9a5ec15365fe4b27
whyttte <fogboruche@yahoo.com>
2020-08-21 00:31:49 +0000

    protecting from malicious wares

--- bootcamp_app/students2.js
+++ bootcamp_app/students2.js
@@ -47,21 +47,46 @@ const results = {};
 
 //.................................................................................................................................
 // IF WE WANNA USE NODE AS INPUT
-pool
-  .query(`
-  SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-  FROM students
-    JOIN cohorts ON cohort_id = cohorts.id
-  WHERE cohorts.name LIKE '%${process.argv[2]}%'
-  LIMIT ${process.argv[3] || 5};
-  `)
-  .then(res => {
-    res.rows.forEach (user => {
-      console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-    })
-  })
-  .catch(err => console.log("query error:", err.stack));
+// pool
+//   .query(`
+//   SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+//   FROM students
+//     JOIN cohorts ON cohort_id = cohorts.id
+//   WHERE cohorts.name LIKE '%${process.argv[2]}%'
+//   LIMIT ${process.argv[3] || 5};
+//   `)
+//   .then(res => {
+//     res.rows.forEach (user => {
+//       console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+//     })
+//   })
+//   .catch(err => console.log("query error:", err.stack));
   
   
-console.log("***End of my Code ***\n");
+// console.log("***End of my Code ***\n");
 
+//........................................................................................................................................
+// TO AVOID MALICIOUS ACTIONS< WE DO THIS INSTEAD
+
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
+
+console.log("***End of my Code ***\n");
